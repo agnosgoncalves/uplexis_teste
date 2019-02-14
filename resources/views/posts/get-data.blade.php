200bc3a855181210470c803a302a86f881f9af1e
agnos <agnos.goncalves@gmail.com>
2019-02-14 13:11:50 -0200

    create folder posts view

---
+++ resources/views/posts/get-data.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <header class="card-header">Capturar artigos</header>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
