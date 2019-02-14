bec1f4eb803af20d9bc2d926dd0e6a28b25a3316
agnos <agnos.goncalves@gmail.com>
2019-02-14 12:54:59 -0200

    create controllers Posts and GetPosts

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/capturar-artigos', 'GetPostsController@index')->name('get-posts');
+Route::get('/artigos', 'PostController@index')->name('posts');
